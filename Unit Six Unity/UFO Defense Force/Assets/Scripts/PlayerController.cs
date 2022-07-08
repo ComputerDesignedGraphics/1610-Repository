c88773c6a35d658f8f79dd92654907691c696e95
Craig Granger <10992413@uvu.edu>
2022-07-07 23:32:50 -0600

    Update

--- Unit Six Unity/UFO Defense Force/Assets/Scripts/PlayerController.cs
+++ Unit Six Unity/UFO Defense Force/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 25;
 
-    public float speed;
-    
+    public float xRange = 30;
+
+    public Transform blaster;
+
+    public GameObject lazerBolt;
     
     // Start is called before the first frame update
     void Start()
@@ -23,5 +27,23 @@ public class PlayerController : MonoBehaviour
         
 //Moves Player left or right by the amount of speed programmed
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        // Keeps player from scrolling off-screen (left side)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Keeps player from scrolling off-screen (right side)
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Create Lazerbolt at the blaster transform position (origination - mainting object rotation) 
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+            
     }
 }
