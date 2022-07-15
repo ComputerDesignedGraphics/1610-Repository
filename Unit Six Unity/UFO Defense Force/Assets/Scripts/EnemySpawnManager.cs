0cc7da9a25111d362952a3fa8f31f08b4dcb079f
Craig Granger <10992413@uvu.edu>
2022-07-14 22:46:47 -0600

    Updates

--- Unit Six Unity/UFO Defense Force/Assets/Scripts/EnemySpawnManager.cs
+++ Unit Six Unity/UFO Defense Force/Assets/Scripts/EnemySpawnManager.cs
@@ -8,17 +8,24 @@ public class EnemySpawnManager : MonoBehaviour
     public GameObject[] ufoPrefabs;
 	private float spawnRangeX = 25f;
 	private float spawnPosZ = 30f;
+	private float startDelay = 2f;  //can change these both to public so they can be changed in the inspector
+	private float spawnInterval = 1.5f;
+
+	void Start()
+	{
+		InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);  //Delay is how long it takes and Spawn Interval is space between
+	}
        
     // Update is called once per frame
     void Update()
     {
-	
-		if(Input.GetKeyDown(KeyCode.S))  //If I press S it sends a ship
- 		{
-			Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
-			int ufoIndex = Random.Range(0,ufoPrefabs.Length);  //Sends Random size UFO from Array - you can add even more ships w/out changing code
-
-        Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //Spawn a indexed UFO from array at random location on X-axis
-		}    
+	    
 	}
+		void SpawnRandomUFO()
+		{
+		Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+		int ufoIndex = Random.Range(0,ufoPrefabs.Length);  //Sends Random size UFO from Array - you can add even more ships w/out changing code
+        Instantiate(ufoPrefabs[ufoIndex],spawnPos, ufoPrefabs[ufoIndex].transform.rotation); //Spawn a indexed UFO from array at random location on X-axis
+		}
+
 }
