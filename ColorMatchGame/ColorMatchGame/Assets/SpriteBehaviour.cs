f075b8abebabea131dddb6f4c964490eabe0107b
Craig Granger <10992413@uvu.edu>
2022-07-31 11:41:54 -0600

    Update

---
+++ ColorMatchGame/ColorMatchGame/Assets/SpriteBehaviour.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class SpriteBehaviour : MonoBehaviour
+{
+    
+	private SpriteRenderer rendererObj;
+
+    private void Start()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+	
+	public void ChangeRendererColor (Color color) 
+	{
+		rendererObj.color = color;
+	}
+
+}
