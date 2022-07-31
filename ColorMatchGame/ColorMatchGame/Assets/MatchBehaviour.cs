8c2a4b6a49d888223c10a609f91871529dd3f3dc
Craig Granger <10992413@uvu.edu>
2022-07-30 22:50:55 -0600

    Update

--- ColorMatchGame/ColorMatchGame/Assets/MatchBehaviour.cs
+++ ColorMatchGame/ColorMatchGame/Assets/MatchBehaviour.cs
@@ -1,20 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MatchBehaviour : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent;
    
     private void OnTriggerEnter(Collider other)
     {
         var tempObj = other.GetComponent<IDContainerBehaviour>();
         if (tempObj == null)
         return;
-
+   
         var otherID = tempObj.idObj;
-        Debug.Log(otherID == idObj ? "Matched" : "No Match");
-        
-        
+        if (otherID == idObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+        }
+
     }
 }
