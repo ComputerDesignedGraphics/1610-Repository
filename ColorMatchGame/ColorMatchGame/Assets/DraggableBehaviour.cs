bfd42e7d868382f60a438763b567ecf7596f7838
Craig Granger <10992413@uvu.edu>
2022-07-31 17:09:41 -0600

    Update -Fixed Bugs "ScreenToViewPoint" fixed in a spot both now read "ScreenToWorldPoint"

---
+++ ColorMatchGame/ColorMatchGame/Assets/DraggableBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DraggableBehaviour : MonoBehaviour
+{
+    private Camera cameraObj;
+    public bool draggable;
+	public Vector3 position, offset;
+
+    void Start()
+    {
+        cameraObj = Camera.main;
+    }
+    public IEnumerator OnMouseDown()
+
+    {
+        offset = transform.position - cameraObj.ScreenToWorldPoint(Input.mousePosition);
+		draggable = true;
+		yield return new WaitForFixedUpdate();
+		
+		
+		
+        while (draggable)
+        {
+            yield return new WaitForFixedUpdate();
+			position = cameraObj.ScreenToWorldPoint(Input.mousePosition) + offset;
+			transform.position = position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        draggable = false;
+    }
+}
