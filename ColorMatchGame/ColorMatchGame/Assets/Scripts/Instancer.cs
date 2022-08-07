120a1edad610a510cedca7adb082357be495ceaa
Craig Granger <10992413@uvu.edu>
2022-08-06 19:43:17 -0600

    7b2 3  3 Update things are working perfectly

--- ColorMatchGame/ColorMatchGame/Assets/Scripts/Instancer.cs
+++ ColorMatchGame/ColorMatchGame/Assets/Scripts/Instancer.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
 public class Instancer : ScriptableObject
-{
-   public void CreateInstance(GameObject obj)
+{	
+	public GameObject prefab;
+    public void CreateInstance()
    {
-       Instantiate(obj);
-   }
+       Instantiate(prefab);
+    }
+	public void CreateInstance(Vector3Data obj)
+	{
+		Instantiate(prefab, obj.value, Quaternion.identity);
+ 	}
+	
 }
+	
+
